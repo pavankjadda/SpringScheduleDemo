0f94f68f9969db1415a6af9b3c2cb5e75a061351
Pavan Kumar Jadda <jaddap2@nih.gov>
2019-10-09 13:09:33 -0400

    first commit

---
+++ src/main/java/com/pj/springscheduledemo/SpringscheduledemoApplication.java
@@ -0,0 +1,15 @@
+package com.pj.springscheduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscheduledemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringscheduledemoApplication.class, args);
+    }
+
+}
